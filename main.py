3539442f75b02a29ef18eaf5000c358d7fcbb2c0
krish2026-del <pal.krishnendu100@gmail.com>
2026-06-22 14:41:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# This tells the browser that DartPad is allowed to talk to this server
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
@@ -12,5 +22,5 @@ async def process_video(file: UploadFile = File(...)):
     return {
         "message": "Video received successfully!",
         "filename": file.filename,
-        "lyrics_and_chords": "[G]                  [Em]\nTomake chueche bhalobashar morsum\n\n[C]                 [D]\nEkhon baje moner bhitor nishpapo nupur"
+        "chords": "[G]                  [Em]\nTomake chueche bhalobashar morsum\n\n[C]                 [D]\nEkhon baje moner bhitor nishpapo nupur"
     }
